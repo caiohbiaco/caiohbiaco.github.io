c21800426fb1b115fe79361339276fb02b5a1b57
𝕮𝖆𝖎𝖔 𝕳𝖊𝖓𝖗𝖎𝖖𝖚𝖊 <caiohbiaco@hotmail.com>
2024-10-01 20:51:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let numero = prompt("Digite um número");
+
+if (numero >0) {
+    console.log("Número positivo");
+} else if (numero <0) {
+    console.log("Número negativo");
+} else { 
+    console.log("Zero");
+}
