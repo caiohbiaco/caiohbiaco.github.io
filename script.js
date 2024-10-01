365457745a616f12c2f37e97ca11f6fd47a3c8c6
Caio Henrique <caiohbiaco@hotmail.com>
2024-10-01 19:52:17 -0300

    aula03 js

--- script.js
+++ script.js
@@ -1,53 +1,42 @@
-var nome = "Santos";
-let idade = 25;
-const cidade = "São Paulo";
+//operadores aritméticos
+let x = 10;
+let y = 5;
 
-console.log("Nome é: ", nome);
-console.log("Idade é: ", idade);
-console.log("Cidade é:", cidade);
+let soma = x + y;
+let subtracao = x - y;
+let multiplicacao = x * y;
+let divisao = x / y;
+let resto = x % y;
 
+//operadores de atribuição
+let a = 10;
+a += 5; //equivalente a: a = a + 5;
+a = a + 5;
 
-//exemplo de objeto
-const pessoa = {
-    nome: "Caio",
-    idade: 23,
-    sexo: "Masculino",
-    telefone: "(42) 66666-6666"
+//operadores de comparação
+let idade = 20;
+let maiorDeIdade = idade >= 18;
+let igual10 = idade == 10;
+
+//estrutura condicional
+const kilo = 10;
+if (kilo >=10) {
+    console.log("Kilo 10+");
+} else {
+    console.log("Kilo -10");
 }
 
-console.log(pessoa);
+//estrutura de repetição
 
-let cachorro = {
-    nome: 'doge',
-    idade: '5',
-    vacinado: false
+//loop (for)
+for (let i = 0; i < 5; i++) {
+    console.log("Interação", i);
 }
 
-cachorro.nome = "Banzudo";
-cachorro.vacinado = true;
+//loop (while)
+let contador = 0;
 
-console.log(cachorro);
-
-//concatenação de strings
-let nomeCompleto = "Caio Henrique";
-let message = "Bem vindo" + nomeCompleto + "ao sistema";
-
-console.log(message);
-
-//operações com numbers
-let a = 333;
-let b = 335;
-let total = a + b;
-
-console.log("O total é:", total);
-
-//conversão para string
-let turma = 1;
-let turmaFormatado = String(turma);
-
-console.log(turmaFormatado);
-
-//conversão para number
-let telefone = "42999999999";
-let telefoneFormatado = Number(telefone);
-console.log(telefoneFormatado);
+while (contador < 5) {
+    console.log("O contador é", + contador);
+    contador++;
+}
