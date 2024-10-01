ad01355015a9937d9563457b84facd3f0d698f01
Caio Henrique <caiohbiaco@hotmail.com>
2024-10-01 20:16:28 -0300

    javascript exercicios

--- script.js
+++ script.js
@@ -1,42 +1,10 @@
-//operadores aritméticos
-let x = 10;
-let y = 5;
 
-let soma = x + y;
-let subtracao = x - y;
-let multiplicacao = x * y;
-let divisao = x / y;
-let resto = x % y;
+let resposta = prompt("Qual a sua idade?");
 
-//operadores de atribuição
-let a = 10;
-a += 5; //equivalente a: a = a + 5;
-a = a + 5;
+    if (resposta >= 18) {
+        console.log("Você é maior de idade");
+    } else {
+        console.log("Você é menor de idade");
+    };
 
-//operadores de comparação
-let idade = 20;
-let maiorDeIdade = idade >= 18;
-let igual10 = idade == 10;
-
-//estrutura condicional
-const kilo = 10;
-if (kilo >=10) {
-    console.log("Kilo 10+");
-} else {
-    console.log("Kilo -10");
-}
-
-//estrutura de repetição
-
-//loop (for)
-for (let i = 0; i < 5; i++) {
-    console.log("Interação", i);
-}
-
-//loop (while)
-let contador = 0;
-
-while (contador < 5) {
-    console.log("O contador é", + contador);
-    contador++;
-}
+console.log("Sua idade é", resposta)
