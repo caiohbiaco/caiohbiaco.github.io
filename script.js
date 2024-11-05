67007dea455401957c59f38b669a491475e2d27f
𝕮𝖆𝖎𝖔 𝕳𝖊𝖓𝖗𝖎𝖖𝖚𝖊 <caiohbiaco@hotmail.com>
2024-11-05 20:44:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-let rotationDegree = 0;
+const personagemContainer = document.getElementById('personagemContainer');
 
-function rotateElement() {
-    rotationDegree +=45;
-    document.getElementById('rotating-div').style
-    .transform = `rotate(${rotationDegree}deg)`;
+function getPersonagem() {
+    console.log();
+
+    fetch("https://rickandmortyapi.com/api/character/?page=19")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`Erro na requisição: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            const arrayPersonagem = data.results;
+            displayPersonagem(arrayPersonagem);
+        })
+        .catch((error) => {
+            console.error();
+        });
 }
 
-function toggleFade() {
-    const div = document.getElementById('fading-div');
-    div.classList.toggle('visible');
+function displayPersonagem(personagens) {
+    personagemContainer.innerHTML = '';
+
+    personagens.forEach((personagem) => {
+        const personagemCard = document.createElement('div');
+        personagemCard.classList.add('personagem-card');
+
+        personagemCard.innerHTML = `
+            <img src="${personagem.image}" alt="${personagem.name}">
+            <h2>${personagem.name}</h2>
+            <p>Status: ${personagem.status}</p>
+            <p>Espécie: ${personagem.species}${personagem.type ? ` - ${personagem.type}` : ''}</p>
+            <p>Origem: ${personagem.origin.name}</p>
+            <p>Localização atual: ${personagem.location.name}</p>
+        `;
+
+        personagemContainer.appendChild(personagemCard);
+    });
 }
 
-function togglePulse() {
-    const div = document.getElementById('pulsing-div');
-    div.classList.toggle('pulsing');
-}
-
-function zoomIn() {
-    const div = document.getElementById('zooming-div');
-    div.classList.add('zooming');
-}
+getPersonagem();
